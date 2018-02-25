f331aab4ea10248d4b941e8802662c8f6d63ef80
Sam <samuel.messina@siu.edu>
2018-02-24 19:33:27 -0600

    exchange graph for average viewer count
    also change get to async

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-let viewer_nums = [];
-let chart = null;
+let total_count = 0;
+let hour_count = 0;
+let total_viewer_nums = [];
+let hour_viewer_nums = [];
 
 function bootstrap() 
 {
@@ -9,15 +11,14 @@ function bootstrap()
   document.getElementById("chat").innerHTML = `<iframe id="iframe" src="https://twitch.tv/${username}/chat?popout="></iframe>`;
 
   let frame = document.getElementById('iframe');
-    frame.onload = function () {
-        let msg = frame.contentWindow.document.querySelector('.message');
-        msg.style.fontSize = '28px';
-        msg.style.lineHeight = '20px';
-    };
+  frame.onload = function () {
+    let msg = frame.contentWindow.document.querySelector('.message');
+    msg.style.fontSize = '28px';
+    msg.style.lineHeight = '20px';
+  };
   // set up the update cycle
   setInterval( function() { 
     updateUsers(); 
-    chart.update();
   }, 10000);
 
   // add listener to input registering "Enter"
@@ -29,14 +30,16 @@ function bootstrap()
   });
 
 }
-
-function httpGet(username="", url="/get_viewers")
+function httpGet(username="" , callback)
 {
-  url = `${url}?username=${username}`;
+  let url = `/get_viewers?username=${username}`;
   let xmlHttp = new XMLHttpRequest();
-  xmlHttp.open( "GET", url, false ); // false for synchronous request. I know, I know....
-  xmlHttp.send( null );
-  return xmlHttp.responseText;
+  xmlHttp.onreadystatechange = function() { 
+    if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+      callback(xmlHttp.responseText);
+  }
+  xmlHttp.open("GET", url, true); // true for asynchronous 
+  xmlHttp.send(null);
 }
 
 function sendUsername() 
@@ -44,47 +47,17 @@ function sendUsername()
   username = document.getElementById("username").value;
   document.getElementById("chat").innerHTML = `<iframe id="iframe" src="https://twitch.tv/${username}/chat?popout="></iframe>`;
   let frame = document.getElementById('iframe');
-    frame.onload = function () {
-        let msg = frame.contentWindow.document.querySelector('.message');
-        msg.style.fontSize = '28px';
-        msg.style.lineHeight = '20px';
-    };
-  viewer_nums = [];
-  chart = createChart();
+  frame.onload = function () {
+    let msg = frame.contentWindow.document.querySelector('.message');
+    msg.style.fontSize = '28px';
+    msg.style.lineHeight = '20px';
+  };
+  total_viewer_nums = [];
+  hour_viewer_nums = [];
   updateUsers(username);
 }
 
 
-function createChart() 
-{
-  return new Chart(document.getElementById("viewer-chart"), {
-    type: 'scatter',
-    data: {
-      datasets: [{ 
-        data: viewer_nums,
-        label: "Viewers",
-        borderColor: "#3e95cd",
-        backgroundColor: "#3e95cd",
-        fill: true,
-        showLine: true
-      }]
-    },
-    options: {   
-      legend: {
-        display: false
-      },
-      tooltips: {
-        callbacks: {
-          label: function(tooltipItem) {
-            return tooltipItem.yLabel;
-          }
-        }
-      },
-      maintainAspectRatio: false
-    }
-  });
-
-}
 
 function updateUsers(username="") 
 {
@@ -93,63 +66,74 @@ function updateUsers(username="")
 
   username = username ? username : document.getElementById("username").value;
 
-  if (!username ) {
-    username = "sscait";
-    document.getElementById("username").value = "sscait";
-  }
-
   // get the viewer json object
-  let result = httpGet(username);
-  result = JSON.parse(result);
+  let result = httpGet(username, (result) => {
+    result = JSON.parse(result);
 
-  // get the viewers and moderators
-  let viewers = result.chatters.viewers;
-  let moderators = result.chatters.moderators;
-  let num_viewers = viewers.length;
-  num_viewers += moderators.length;
+    // get the viewers and moderators
+    let viewers = result.chatters.viewers;
+    let moderators = result.chatters.moderators;
+    let num_viewers = viewers.length;
+    num_viewers += moderators.length;
 
 
-  // calculate the size of the boxes based on the number of viewers
-  let num_per_row = Math.floor(window.innerWidth / 2 / 150);
-  num_per_row = num_per_row < num_viewers ? num_per_row: num_viewers;
+    // calculate the size of the boxes based on the number of viewers
+    let num_per_row = Math.floor(window.innerWidth / 2 / 150);
+    num_per_row = num_per_row < num_viewers ? num_per_row: num_viewers;
 
-  let height = 100 / Math.ceil(num_viewers / num_per_row) - 2;
-  let width = num_viewers < 4 ? `100${100/num_viewers}%` : "150px";
+    let height = 100 / Math.ceil(num_viewers / num_per_row) - 2;
+    let width = num_viewers < 4 ? `100${100/num_viewers}%` : "150px";
 
-  // colors for our viewer boxes
-  let color_array = [ "#5bc0eb", "#fde74c", "#9bc53d", "#e55934", "#fa7921" ];
+    // colors for our viewer boxes
+    let color_array = [ "#5bc0eb", "#fde74c", "#9bc53d", "#e55934", "#fa7921" ];
 
-  // create our li elements
-  let i = 0;
-  for (let viewer of viewers) {
-    let color = color_array[i++ % 5];
-    user_list.push(`<li style="min-height: ${height}%; 
+    // create our li elements
+    console.log("end1");
+    let i = 0;
+    for (let viewer of viewers) {
+      let color = color_array[i++ % 5];
+      user_list.push(`<li style="min-height: ${height}%; 
                     width=${width}; 
                     background-color: ${color}">
                       <p>${viewer}</p>
                     </li>`);
-  }
+    }
 
-  for (let viewer of moderators) {
-    let color = color_array[i++ % 5];
-    user_list.push(`<li style="min-height: ${height}%; 
+    for (let viewer of moderators) {
+      let color = color_array[i++ % 5];
+      user_list.push(`<li style="min-height: ${height}%; 
                     width=${width}; 
                     background-color: ${color}; 
                     font-weight: bold;" 
                     class="mod">
                       <p>${viewer}</p>
                       </li>`);
-  }
+    }
 
 
-  // update the object used for the graph
-  viewer_nums.push({x:viewer_nums.length, y:num_viewers});
+    // update the object used for the graph
+    if (hour_viewer_nums.length == 360) {
+      hour_count = hour_count - hour_viewer_nums[0]
+      hour_viewer_nums.shift();
+    }
+    total_count += num_viewers;
+    hour_count += num_viewers;
+    total_viewer_nums.push(num_viewers);
+    hour_viewer_nums.push(num_viewers);
+    let total_avg = Math.round(total_count / total_viewer_nums.length, 2);
+    let hour_avg = Math.round(hour_count / hour_viewer_nums.length, 2);
+    console.log(`total: ${total_avg}`);
+    console.log(`hour: ${hour_avg}`);
 
-  // update the html
-  document.getElementById("container").innerHTML = user_list.join("");
-  document.getElementById("num-viewers").innerHTML = ": " + num_viewers;
+    // update the html
+    document.getElementById("container").innerHTML = user_list.join("");
+    document.getElementById("num-viewers").innerHTML = ": " + num_viewers;
+    document.getElementById("total-avg").innerHTML = total_avg
+    document.getElementById("last-hour-avg").innerHTML = total_avg
+    return;
+  });
   return;
 }
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
 
-chart = createChart();
 bootstrap();
